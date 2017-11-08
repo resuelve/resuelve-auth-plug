11a2dd736469ec80063a8f6aa0e732b44dc7ad82
lnx1337 <dev.lnx1337@gmail.com>
2017-11-08 01:23:34 -0600

    refactor test

--- test/resuelve_auth_test.exs
+++ test/resuelve_auth_test.exs
@@ -3,20 +3,21 @@ defmodule ResuelveAuthTest do
   alias ResuelveAuth.Helpers.TokenHelper
   alias ResuelveAuth.Helpers.TokenData
 
-  @token "eyJ0aW1lc3RhbXAiOiJ0aW1lc3RhbXAiLCJzZXNzaW9uIjoic2Vzc2lvbiIsInJvbGUiOiJyb2xlIiwibWV0YSI6Im1ldGFkYXRhIn0=.C50004F5D0FE64E06C03200258A96447543925AFE7D8FDC1684D5A275E1E3E30"
+  @json "eyJ0aW1lc3RhbXAiOiJ0aW1lc3RhbXAiLCJzZXNzaW9uIjoic2Vzc2lvbiIsInJvbGUiOiJyb2xlIiwibWV0YSI6Im1ldGFkYXRhIn0=."
+  @sign "C50004F5D0FE64E06C03200258A96447543925AFE7D8FDC1684D5A275E1E3E30"
 
-  test "generate new token" do
+  test "Generate new token" do
     token_data = %TokenData{
       role: "role",
       session: "session",
       timestamp: "timestamp",
       meta: "metadata"
     }
-    assert TokenHelper.create_token(token_data, "secret") == @token
+    assert TokenHelper.create_token(token_data, "secret") == (@json <> @sign)
   end
 
-  test "verify token" do
-    assert TokenHelper.verify_token(@token, "secret") == {
+  test "Verify token" do
+    assert TokenHelper.verify_token(@json <> @sign, "secret") == {
       :ok,
       %{
         "meta" => "metadata",

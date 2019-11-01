e80d196be8dfb6a80fa912ddbd6f4b64d36c1b0b
Ivan Jaimes <ivan.jaimes@resuelve.mx>
2019-11-01 17:13:28 -0600

    Fix unit time error

--- lib/helpers/token_helper.ex
+++ lib/helpers/token_helper.ex
@@ -7,6 +7,7 @@ defmodule ResuelveAuth.Helpers.TokenHelper do
   alias ResuelveAuth.Helpers.TokenData
   alias ResuelveAuth.Utils.Calendar
   @error "Unauthorized"
+  @limit_time 4
 
   @doc """
   Genera un token usando un mapa. Retorna un token con el siguiente formato
@@ -14,7 +15,7 @@ defmodule ResuelveAuth.Helpers.TokenHelper do
   """
   @spec create_token(struct, String.t()) :: String.t()
   def create_token(%TokenData{} = data, secret) when is_map(data) do
-    no_logs() || Logger.debug("Token data: #{inspect(data)}")
+    Logger.debug("Token data: #{inspect(data)}")
 
     case Poison.encode(data) do
       {:ok, json} ->
@@ -32,6 +33,8 @@ defmodule ResuelveAuth.Helpers.TokenHelper do
   """
   @spec verify_token(String.t(), String.t()) :: tuple
   def verify_token(token, secret) do
+    IO.puts("Token: #{token} secret: #{secret}")
+
     case String.contains?(token, ".") do
       true -> verify_token(token, secret, :ok)
       false -> {:error, @error}
@@ -46,7 +49,13 @@ defmodule ResuelveAuth.Helpers.TokenHelper do
     |> equivalent?()
     |> parse_token_data()
     |> expired?()
+    |> debug()
     |> response()
+  end
+
+  def debug(input) do
+    IO.puts("Debug: #{inspect(input)}")
+    input
   end
 
   def sign_data(secret, data, sign) do
@@ -58,12 +67,24 @@ defmodule ResuelveAuth.Helpers.TokenHelper do
     Base.encode16(:crypto.hmac(:sha256, secret, data))
   end
 
-  def equivalent?(%{data: data, valid_sign: valid_sign, sign: sign}) do
+  def equivalent?(%{data: data, valid_sign: valid_sign, sign: sign} = params) do
+    IO.puts("Params #{inspect(params)}")
+
     if String.equivalent?(valid_sign, sign) do
+      IO.puts("Algo sale bien: #{inspect(data)}")
       {:ok, data}
     else
+      IO.puts("Algo sale mal")
       {:error, false}
     end
+  end
+
+  def parse_token_data({:error, message}), do: {:error, message}
+
+  def parse_token_data({:ok, data}) do
+    {:ok, json} = Base.url_decode64(data)
+    IO.puts("Parse_token: #{inspect(json)}")
+    Poison.decode(json)
   end
 
   def expired?({:error, _}), do: {:error, @error}
@@ -72,6 +93,8 @@ defmodule ResuelveAuth.Helpers.TokenHelper do
     expired =
       data
       |> Map.get("timestamp")
+      |> Calendar.add(@limit_time, :hour)
+      |> debug()
       |> Calendar.is_past?()
 
     %{expired: expired, data: data}
@@ -81,17 +104,8 @@ defmodule ResuelveAuth.Helpers.TokenHelper do
   def response(%{expired: false, data: data}), do: {:ok, data}
   def response({:error, message}), do: {:error, message}
 
-  def parse_token_data({:error, message}), do: {:error, message}
-
-  def parse_token_data({:ok, data}) do
-    {:ok, json} = Base.url_decode64(data)
-    Poison.decode(json)
-  end
-
   defp build_token(encoded_json, secret) do
     sign = sign_data(secret, encoded_json)
     "#{encoded_json}.#{sign}"
   end
-
-  defp no_logs, do: Application.get_env(:resuelve_auth, :no_logs)
 end

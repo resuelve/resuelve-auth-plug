9aaa1f3fadb5aa6f1ea8d274a09378350c82a810
Ivan Jaimes <ivan.jaimes@resuelve.mx>
2020-07-03 08:36:57 -0500

    Change test to english

--- test/utils/calendar_test.exs
+++ test/utils/calendar_test.exs
@@ -1,6 +1,6 @@
 defmodule ResuelveAuth.Utils.CalendarTest do
   @moduledoc """
-  Módulo encargado de las pruebas del módulo de Calendar.
+  Testing the calendar module.
   """
   use ExUnit.Case
   doctest ResuelveAuth.Utils.Calendar

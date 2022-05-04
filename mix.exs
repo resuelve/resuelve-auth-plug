f7cee0d8612e03a865c9c89c46cd3afbd21ae279
Francisco Flores <franciscocapipo@gmail.com>
2022-05-03 23:27:40 -0700

    Actualiza credo

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule ResuelveAuth.Mixfile do
       {:excoveralls, "~> 0.12", only: :test, override: true},
       {:ex_doc, ">= 0.19.0", runtime: false, override: true},
       {:earmark, "~> 1.3.0", override: true},
-      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:poison, "~> 3.1", override: true},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]

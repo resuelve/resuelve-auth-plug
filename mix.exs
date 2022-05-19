41a3af28c286084ce68f9af4f7b1278855f63982
Francisco Flores <franciscocapipo@gmail.com>
2022-05-18 22:55:25 -0700

    Quita `override: true` que impedían publicar el paquete

--- mix.exs
+++ mix.exs
@@ -56,10 +56,10 @@ defmodule ResuelveAuth.Mixfile do
     [
       {:plug, "~> 1.8"},
       {:excoveralls, "~> 0.12", only: :test, override: true},
-      {:ex_doc, ">= 0.19.0", runtime: false, override: true},
-      {:earmark, "~> 1.3.0", override: true},
+      {:ex_doc, ">= 0.19.0", runtime: false},
+      {:earmark, "~> 1.3.0"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:poison, "~> 3.1", override: true},
+      {:poison, "~> 3.1"},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
